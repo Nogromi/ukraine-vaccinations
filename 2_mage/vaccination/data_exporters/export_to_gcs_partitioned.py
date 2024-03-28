0d8a74534f4d62506ec5b8bbc5eeb90734374fcb
Nogromi <anatolii.krivko@gmail.com>
2024-03-28 18:25:37 +0200

    orchestration

---
+++ 2_mage/vaccination/data_exporters/export_to_gcs_partitioned.py
@@ -0,0 +1,39 @@
+if 'data_loader' not in globals():
+    from mage_ai.data_preparation.decorators import data_loader
+if 'test' not in globals():
+    from mage_ai.data_preparation.decorators import test
+import pyarrow as pa
+import pyarrow.parquet as pq 
+import os
+
+os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = '/home/src/sa-vaccination-mage.json'
+
+@data_loader
+def load_data(*args, **kwargs):
+    """
+    Template code for loading data from any source.
+
+    Returns:
+        Anything (e.g. data frame, dictionary, array, int, str, etc.)
+    """
+    # Specify your data loading logic here
+    bucket_name = 'vaccination-ukraine-1'
+    project_id = 'vaccination-ukraine-1'
+
+    table_name = 'immunizations_covid19_4qrt_2023'
+
+    root_path = f'{bucket_name}/{table_name}'
+
+    gcs = pa.fs.GcsFileSystem()  # so here's pyarrow will automatically connect with your credentials that you set above
+
+    arrow_df = pq.ParquetDataset(root_path, filesystem=gcs)
+    df = arrow_df.read_pandas().to_pandas()
+    return df
+
+
+@test
+def test_output(output, *args) -> None:
+    """
+    Template code for testing the output of the block.
+    """
+    assert output is not None, 'The output is undefined'
---
+++ 2_mage/vaccination/data_loaders/get_from_diia.py
@@ -0,0 +1,59 @@
+import io
+import pandas as pd
+import requests
+if 'data_loader' not in globals():
+    from mage_ai.data_preparation.decorators import data_loader
+if 'test' not in globals():
+    from mage_ai.data_preparation.decorators import test
+
+
+@data_loader
+def load_data_from_api(*args, **kwargs):
+    """
+    Template for loading data from API
+    """
+    url = ''
+    response = requests.get(url)
+
+    return pd.read_csv(io.StringIO(response.text), sep=',')
+
+
+@data_loader
+def load_data_from_api(*args, **kwargs):
+    """
+    Template for loading data from API
+    """
+    download_url=kwargs.get('url')
+    data_dtypes=    {
+        'temp_immunization_id': str,
+        'legal_entity_id': str,
+        'division_identifier_value': str,
+        'status': str,
+        'not_given': bool,
+        'vaccine_code': str,
+        'patient_age_group': str,
+        'patient_gender': str,
+        'manufacturer': str,
+        'lot_number': str,
+        'dose_quantity_unit': str,
+        'dose_quantity_value': float,
+        'vaccination_protocol_dose_sequence': pd.Int64Dtype(),
+        'vaccination_protocol_series': str,
+        'vaccination_protocol_series_doses': pd.Int64Dtype(),
+        'vaccination_protocol_target_diseases': str
+        }
+    parse_dates=['immunization_date','expiration_date','inserted_at','updated_at']
+    df = pd.read_csv(download_url, compression='zip', header=0, sep=',', quotechar='"', dtype=data_dtypes , parse_dates=parse_dates)
+    # df = pd.read_csv(download_url, compression='zip', header=0, sep=',', quotechar='"')
+    print(df.shape)
+    print(df.info())
+    return df
+
+
+
+@test
+def test_output(output, *args) -> None:
+    """
+    Template code for testing the output of the block.
+    """
+    assert output is not None, 'The output is undefined'
